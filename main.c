1cce3e364d87d98fc46cd8ceb4b123fabbf4096e
Kinidario <102186057+Kinidario@users.noreply.github.com>
2022-03-23 11:10:31 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,14 @@
+//Bibliotecas necessárias
 #include <stdio.h>
 #include<math.h>
 
+//Função que calcula o número de binários
 int Binario(int n){
+//Variáveis da função
   int cont = 0, soma = 0, i=0;
+//Estrutura de repetição
   while(1){
+// A contagem de quantidade de '1' começa achando a potência de 2 mais próxima de n e que não seja maior que n.
     if(pow(2,i) > n )
       break;
     
@@ -11,28 +16,34 @@ int Binario(int n){
   }
 
   i = i - 1;
-  
+// Se o número for ímpar o contador já soma 1 por causa da potência 2^0  
   if(n%2 != 0)
     cont = cont + 1;
-  
+//Estrutura de repetição que vai de i até 1
   for(i; i > 0; i=i-1 ){
     soma = soma + pow(2,i);
+//Se a soma passar do valor de n, então o código remove aquela soma e passa para uma potência de 2 menor
     if(soma > n){
       soma = soma - pow(2,i);
       continue;
     }
+//Se a soma não passar então o contador soma 1
     else
      cont = cont + 1;
   }
+//Retorna o valor do contador
   return cont;
 }
 
+//Função principal
 int main(void) {
-  int n, i=0;
+  int n;
   
+//Recebendo as variáveis do teclado
   printf("digite um numero : ");
   scanf("%d", &n);
 
+//Chamando a função e printando o número de 1.
   printf("%d", Binario(n));
   
   return 0;
